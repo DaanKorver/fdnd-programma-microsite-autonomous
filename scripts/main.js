90c1ddb9280c23f54fff1957508fb22713be789b
DaanKorver <daank2000@hotmail.nl>
2021-10-07 10:20:01 +0200

    added drag to scroll

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,31 @@
 import semester_sprint from "./semester_sprint.js";
 
 const ctas = document.querySelectorAll(".cta")
+const navbar = document.querySelector(".nav__bar")
+let mouseDown = false
+let startX, scrollLeft
+
+let startDragging = function (e) {
+  mouseDown = true;
+  startX = e.pageX - navbar.offsetLeft;
+  scrollLeft = navbar.scrollLeft;
+};
+let stopDragging = function (event) {
+  mouseDown = false;
+};
+
+navbar.addEventListener('mousemove', (e) => {
+  e.preventDefault();
+  if(!mouseDown) { return; }
+  const x = e.pageX - navbar.offsetLeft;
+  const scroll = x - startX;
+  navbar.scrollLeft = scrollLeft - scroll;
+});
+
+// Add the event listeners
+navbar.addEventListener('mousedown', startDragging, false);
+navbar.addEventListener('mouseup', stopDragging, false);
+navbar.addEventListener('mouseleave', stopDragging, false);
 
 
 // Getting for JSON
