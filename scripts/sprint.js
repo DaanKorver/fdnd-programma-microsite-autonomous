8e97e0edd796577407211e0fa5fde353f7939964
DaanKorver <daank2000@hotmail.nl>
2021-10-08 17:19:53 +0200

    WIP

--- scripts/sprint.js
+++ scripts/sprint.js
@@ -3,4 +3,16 @@ import sprints from "./sprints.js"
 const sprintNummer = (new URLSearchParams(window.location.search).get("sprint")) - 1
 const pageContent = sprints[sprintNummer]
 
+const title = document.getElementById("title")
+const description = document.getElementById("description")
+const links = document.querySelectorAll("#links > li > a")
+
 console.log(pageContent);
+
+
+title.innerText = pageContent.titel
+description.innerText = pageContent.beschrijving
+
+for (let i = 0; i < links.length; i++) {
+    links[i].href = pageContent.opdrachten[i].live
+}
