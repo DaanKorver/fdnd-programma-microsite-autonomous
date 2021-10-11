b927249fd364b1dbab4de9e7ff4766d9da4c0ff8
DaanKorver <daank2000@hotail.nl>
2021-10-11 17:09:04 +0200

    Optimization

--- scripts/main.js
+++ scripts/main.js
@@ -2,45 +2,74 @@ import sprints from "./sprints.js";
 
 const sprintsContainer = document.querySelector(".sprints-semesters")
 
-// sprintsContainer.insertAdjacentHTML("")
 
-sprints.forEach(sprint=>{
-  sprintsContainer.innerHTML += 
-`
-<section id="sprint${sprint.sprint}" class="sprint">
-<div class="sprint-image" style="background-image: url('${sprint.foto}')">
-    <div class="sprint-title">
-        <h2>Sprint</h2>
-        <div class="sprint-number">${sprint.sprint}</div>
-    </div>
-</div>
-<div class="sprint-components">
-    <button class="cta">learn</button>
-    <p class="sprint-name">${sprint.naam}</p>
-</div>
+async function avifSupport() {
+    return new Promise((resolve, reject)=>{
+        const img = new Image()
+        let avifSupport = true
+        img.src = '../assets/sprint-images/content-first.avif'
+        img.onerror = ()=>{
+            avifSupport = false
+            reject(avifSupport)
+        }
+        img.onload = ()=>{
+            resolve(avifSupport)
+        }
+    })
+}
 
-<div class="sprint-components-bottom">
-    <div class="heart-container">
-        <svg class="heart" viewBox="0 0 32 29.6">
-            <path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
-          c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z" />
-        </svg>
-    </div>
-    <div class="hashtag-container">
-        <svg class="hashtag" version="1.1" id="Capa_2" xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 490 490"
-            style="enable-background:new 0 0 490 490;" xml:space="preserve">
-            <path d="M64.333,490h58.401l33.878-137.69h122.259L245.39,490h58.401l33.878-137.69h119.92v-48.162h-108.24l29.2-117.324h79.04
-v-48.162H390.23L424.108,0H365.31l-33.878,138.661H208.79L242.668,0h-58.415l-33.864,138.661H32.411v48.162h106.298l-28.818,117.324
-h-77.48v48.162h65.8L64.333,490z M197.11,186.824h122.642l-29.2,117.324H168.292L197.11,186.824z" />
-        </svg>
-    </div>
-    <div class="arrow-container">
-        <a href="sprint.html?sprint=${sprint.sprint}">
-            <div class="arrow"></div>
-        </a>
-    </div>
-</div>
-</section>
-`
+avifSupport().then(()=>{
+    renderSprints()
+}).catch(()=>{
+    renderSprints("noSupport")
+    fixImages()
 })
+
+function renderSprints(support) {
+    sprints.forEach(sprint=>{
+        sprintsContainer.innerHTML += 
+      `
+      <section id="sprint${sprint.sprint}" class="sprint">
+      <div class="sprint-image" style="background-image: url('${!support ? sprint.foto : sprint.foto.split(".avif").shift() + ".jpg"}')">
+          <div class="sprint-title">
+              <h2>Sprint</h2>
+              <div class="sprint-number">${sprint.sprint}</div>
+          </div>
+      </div>
+      <div class="sprint-components">
+          <button class="cta">learn</button>
+          <p class="sprint-name">${sprint.naam}</p>
+      </div>
+      
+      <div class="sprint-components-bottom">
+          <div class="heart-container">
+              <svg class="heart" viewBox="0 0 32 29.6">
+                  <path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
+                c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z" />
+              </svg>
+          </div>
+          <div class="hashtag-container">
+              <svg class="hashtag" version="1.1" id="Capa_2" xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 490 490"
+                  style="enable-background:new 0 0 490 490;" xml:space="preserve">
+                  <path d="M64.333,490h58.401l33.878-137.69h122.259L245.39,490h58.401l33.878-137.69h119.92v-48.162h-108.24l29.2-117.324h79.04
+      v-48.162H390.23L424.108,0H365.31l-33.878,138.661H208.79L242.668,0h-58.415l-33.864,138.661H32.411v48.162h106.298l-28.818,117.324
+      h-77.48v48.162h65.8L64.333,490z M197.11,186.824h122.642l-29.2,117.324H168.292L197.11,186.824z" />
+              </svg>
+          </div>
+          <div class="arrow-container">
+              <a href="sprint.html?sprint=${sprint.sprint}">
+                  Bekijk deze sprint
+                  <div class="arrow"></div>
+              </a>
+          </div>
+      </div>
+      </section>
+      `
+      })
+}
+
+function fixImages() {
+    document.querySelector(".home").style.backgroundImage = `url('../assets/hero-image.jpeg')`
+    document.querySelector(".opdrachten-leertaken-image").style.backgroundImage = `url('../assets/kickoff-3.JPG')`
+}
--- scripts/sprints.js
+++ scripts/sprints.js
@@ -5,7 +5,7 @@ export default [
     "titel": "Schetsen, Ontwerpen, bouwen en Intergreren. Alles leren over coden en front-end.",
     "datum": "6 september - 24 september",
     "beschrijving": "De opleiding FDND bestaat uit een tribe met daarin een aantal squads, in een squad zitten studenten en docenten. Het eerste semester begint met de sprint Your Tribe: presenteer jezelf en je squad met een eigen website. In deze sprint maak je kennis met je studiegenoten en de opleiding. Je leert je eigen online profielpagina maken en gaat met je Squad een gezamenlijke website maken.De focus in deze sprint ligt op kennismaken, samenwerken en hoe je interface en vormgevingsprincipes kan toepassen voor een responsive design.",
-    "foto": "../assets/sprint-images/your-tribe.jpg",
+    "foto": "../assets/sprint-images/your-tribe.avif",
     "opdrachten": [
       {
         "team": 1,
@@ -33,7 +33,7 @@ export default [
     "titel": "De opdrachtgever helpen naar zijn doel.",
     "datum": "27 september - 15 oktober",
     "beschrijving": "In de tweede sprint, The Client, leer je hoe je een opdracht voor een klant kan aanpakken. De opdracht die je krijgt, kan een hele website zijn, of een onderdeel van een bestaande website. Je gaat bedrijven bezoeken in Amsterdam, misschien ga je al een paar dagen bij een bedrijf werken aan een opdracht.De focus van deze sprint ligt op het maken van een opdracht voor een opdrachtgever en het presenteren van het resultaat.",
-    "foto": "../assets/sprint-images/the-client.jpg",
+    "foto": "../assets/sprint-images/the-client.avif",
     "opdrachten": [
       {
         "team": 1,
@@ -61,7 +61,7 @@ export default [
     "titel": "Beperkingen zijn geen beperkingen.",
     "datum": "18 oktober - 5 november",
     "beschrijving": "De sprint All human gaat over de mensen waarvoor we websites maken. Hoe kun je een website maken die verschillende mensen, met verschillende apparaten, in verschillende situaties goed kunnen gebruiken? Mensen met grote schermen, mensen met kleine schermen, slechtziende mensen, mensen met een gebroken arm, mensen met langzame computers of een verouderde mobiel …De focus van deze sprint ligt op gebruiksvriendelijkheid, toegankelijkheid, testen en de testresultaten gebruiken voor het verbeteren van een ontwerp.",
-    "foto": "../assets/sprint-images/all-human.jpg",
+    "foto": "../assets/sprint-images/all-human.avif",
     "opdrachten": [
       {
         "team": 1,
@@ -89,7 +89,7 @@ export default [
     "titel": "Maak de huisstijl compleet.",
     "datum": "15 november - 3 december",
     "beschrijving": "In de vierde sprint, The Campaign help je een bedrijf of organisatie met een online campagne. Je leert onderdelen van een website maken binnen een bestaande huisstijl en hoe je mensen kunt verleiden. De focus van deze sprint ligt op het toepassen van een huisstijl en maken van formulieren.",
-    "foto": "../assets/sprint-images/the-campaign.jpg",
+    "foto": "../assets/sprint-images/the-campaign.avif",
     "opdrachten": [
       {
         "team": 1,
@@ -117,7 +117,7 @@ export default [
     "titel": "Kan je me helpen navigeren?",
     "datum": "6 december - 24 december",
     "beschrijving": "In de sprint Fix the Flow ga je leren over navigatie systemen, hoe je informatie op een goede manier kan presenteren en hoe gebruikers deze data kunnen filteren en sorteren.",
-    "foto": "../assets/sprint-images/fix-the-flow.jpg",
+    "foto": "../assets/sprint-images/fix-the-flow.avif",
     "opdrachten": [
       {
         "team": 1,
@@ -145,7 +145,7 @@ export default [
     "titel": "Business is a combination of war and sport.",
     "datum": "10 januari - 28 januari",
     "beschrijving": "In de sprint The Startup ga je zelf of met een team een bedrijf helpen met een website of online concept. Je hebt dit semester al heel veel geleerd, in deze sprint ga je dit toepassen voor een opdrachtgever.",
-    "foto": "../assets/sprint-images/the-startup.jpg",
+    "foto": "../assets/sprint-images/the-startup.avif",
     "opdrachten": [
       {
         "team": 1,
@@ -173,7 +173,7 @@ export default [
     "titel": "Let's connect.",
     "datum": "",
     "beschrijving": "In de sprint Connect Your Tribe bouw je een infrastructuur waarmee je ook na de opleiding in contact kunt blijven met je Squad. Je hebt al een website van je squad met alle digitale visitekaartjes. Maak van deze website een social network. Misschien kunnen mensen een reactie plaatsen bij een visitekaartjes, of een status-update invoeren.",
-    "foto": "../assets/sprint-images/connect-your-tribe.jpg",
+    "foto": "../assets/sprint-images/connect-your-tribe.avif",
     "opdrachten": [
       {
         "team": 1,
@@ -201,7 +201,7 @@ export default [
     "titel": "Houdt de controle in jouw handen.",
     "datum": "",
     "beschrijving": "Bij het maken van een data-driven toepassing is het belangrijk je gebruikers te informeren over de status zodat zij weten wat ze moeten doen. In de sprint Keep Users In Control leer je hoe je externe data kunt laden en toont in een webpagina waarbij de gebruiker in controle blijft.",
-    "foto": "../assets/sprint-images/keep-users-in-control.jpg",
+    "foto": "../assets/sprint-images/keep-users-in-control.avif",
     "opdrachten": [
       {
         "team": 1,
@@ -229,7 +229,7 @@ export default [
     "titel": "Het web is voor iedereen.",
     "datum": "",
     "beschrijving": "De kracht van het internet ligt in haar universaliteit. In de sprint The Web is for Everyone leer je hoe je robuuste, toegankelijke websites maakt voor alle wezens in het universum.",
-    "foto": "../assets/sprint-images/the-web-is-for-everyone.jpg",
+    "foto": "../assets/sprint-images/the-web-is-for-everyone.avif",
     "opdrachten": [
       {
         "team": 1,
@@ -257,7 +257,7 @@ export default [
     "titel": "Optimaliseer je website.",
     "datum": "",
     "beschrijving": "In de sprint Content First leer je wat je kunt doen om webapplicaties sneller te maken en te optimaliseren. Ook leer je hoe je ervoor kan zorgen dat je applicatie werkt als er geen verbinding is met het internet.",
-    "foto": "../assets/sprint-images/content-first.jpg",
+    "foto": "../assets/sprint-images/content-first.avif",
     "opdrachten": [
       {
         "team": 1,
@@ -285,7 +285,7 @@ export default [
     "titel": "Verbind jezelf met iedereen.",
     "datum": "",
     "beschrijving": "Sprint 11 gaat over Connecting People, het web verbindt mensen met elkaar. Mensen kunnen online berichten sturen, met elkaar in discussie gaan en samenwerken in gedeelde documenten. Tijdens deze sprint leer je real-time applicaties waarmee mensen direct met elkaar in verbinding staan.",
-    "foto": "../assets/sprint-images/connecting-people.jpg",
+    "foto": "../assets/sprint-images/connecting-people.avif",
     "opdrachten": [
       {
         "team": 1,
@@ -313,7 +313,7 @@ export default [
     "titel": "Proof that you are worthy.",
     "datum": "",
     "beschrijving": "In de sprint Proof of Concept ga je zelf of met een team een opdrachtgever helpen met een data-driven online concept. Je hebt dit jaar heel veel geleerd, in deze sprint laat je zien wat jij alleen én in een team kunt bereiken in 3 weken.",
-    "foto": "../assets/sprint-images/proof-of-concept.jpg",
+    "foto": "../assets/sprint-images/proof-of-concept.avif",
     "opdrachten": [
       {
         "team": 1,
@@ -341,7 +341,7 @@ export default [
     "titel": "Breidt je verbinding uit.",
     "datum": "",
     "beschrijving": "Je hebt al een basis social network voor jouw squad. Breid dit social network uit naar een platform waarop de tribe members elkaar kan vinden en ondersteunen. In ieder geval gedurende het begin van jullie carriere als frontend designer & developer.",
-    "foto": "../assets/sprint-images/your-tribe-for-life.jpg",
+    "foto": "../assets/sprint-images/your-tribe-for-life.avif",
     "opdrachten": [
       {
         "team": 1,
@@ -369,7 +369,7 @@ export default [
     "titel": "Maak je keuze.",
     "datum": "",
     "beschrijving": "Tijdens deze sprint ga je leren hoe je met een goede projectinrichting volgens de dev-lifecycle met behulp van een nieuwe framework een opdracht realiseert.",
-    "foto": "../assets/sprint-images/choices-choices.jpg",
+    "foto": "../assets/sprint-images/choices-choices.avif",
     "opdrachten": [
       {
         "team": 1,
@@ -397,7 +397,7 @@ export default [
     "titel": "Samen sta je sterker.",
     "datum": "",
     "beschrijving": "In een team ga je aan de hand van een component framework werken aan de opracht van een opdrachtgever.",
-    "foto": "../assets/sprint-images/team-up.jpg",
+    "foto": "../assets/sprint-images/team-up.avif",
     "opdrachten": [
       {
         "team": 1,
@@ -425,7 +425,7 @@ export default [
     "titel": "En we gaan weer terug.",
     "datum": "",
     "beschrijving": "Na server side renderen kwamen de single page applications, oftwel renderen in de client en nu zijn we weer terug bij het serveren van statische html, alleen nu met ene build en pre-rendering fase ertussen. Agile en efficient een dynamische web site opzetten. Er blijft hierdoor ruimte om te focussen op creative coding.",
-    "foto": "../assets/sprint-images/back-to-static.jpg",
+    "foto": "../assets/sprint-images/back-to-static.avif",
     "opdrachten": [
       {
         "team": 1,
@@ -453,7 +453,7 @@ export default [
     "titel": "Hou je hoofd erbij.",
     "datum": "",
     "beschrijving": "Een headless cms in combinatie met serverless functions maakt het mogelijk om websites te bouwen zonder een server op te hoeven tuigen. Hiermee hoef je je niet te richten op server configuratie en onderhoud (denk aan het up-to-date houden van al je npm packages), maar kan je je volledig richten op de interface en de gebruiker.",
-    "foto": "../assets/sprint-images/lose-your-head.jpg",
+    "foto": "../assets/sprint-images/lose-your-head.avif",
     "opdrachten": [
       {
         "team": 1,
@@ -481,7 +481,7 @@ export default [
     "titel": "Laten we feesten.",
     "datum": "",
     "beschrijving": "Moderen frontend applicaties maak je met de JAM Stack. Met deze architectuur maak je een moderne, snelle, toegankelijke en veilige web applicatie voor een opdrachtgever.",
-    "foto": "../assets/sprint-images/lets-jam.jpg",
+    "foto": "../assets/sprint-images/lets-jam.avif",
     "opdrachten": [
       {
         "team": 1,
